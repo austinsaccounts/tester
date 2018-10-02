3e1f76a4d719828cd9b34863ec8c048adf985135
austin cummings and ben kiggen <austin@>
2018-10-02 11:49:48 -0700

    basic tests

--- spec/roman-spec.js
+++ spec/roman-spec.js
@@ -1,9 +1,29 @@
-// describe('roman', function(){
-//
-//   it('should return a string') do     expect(23.backEnd).to(eq("XXIII"))
-//   });
+import {backEnd} from './../src/romanNumerals.js';
 
-//
-// it('returns false for numbers not divisible by four') do
-//     expect(1999.isLeapYear).to(eq(false))
-//   end
+describe('backEnd', function() {
+
+    it('should return a string', function(){
+    var answer = backEnd(5);
+    expect(typeof answer).toEqual("string");
+  });
+
+  it('should recognize a single number and return the roman numeral', function(){
+  var answer = backEnd(5);
+  expect(answer).toEqual("V");
+});
+  it('should return a roman numerla', function(){
+  var answer = backEnd(5);
+  expect(answer).toEqual("V");
+  });
+
+  it('should return a string', function(){
+  var answer = backEnd(5);
+  expect(answer).toEqual("V");
+  });
+
+
+
+  });
+
+
+//https://jasmine.github.io/tutorials/your_first_suite
